fa4aaa8e08aa979947908e38ea06dbe9047a13d5
Krista DeStasio <kdestasio@gmail.com>
2019-05-14 13:25:26 -0700

    for loop to create scatterplot for each km solution

--- clustering/4_kmeans_visualizations.R
+++ clustering/4_kmeans_visualizations.R
@@ -8,10 +8,22 @@ library(purrr)
 
 setwd(here())
 
+# PCA data
+load("output/data_pca3_rt.Rda")
+load("output/data_pca3_bias.Rda")
+
+# K-means clustering solutions
 load("output/data_km_pca3_rt.Rda")
 load("output/data_km_pca3_bias.Rda")
 
 # Scatterplot colored by cluster membership -------------------------------------
-
+for (i in seq(1, length(km_data_pca3_bias))) {
+    num_clusts <- length(unique(km_data_pca3_bias[[i]]$cluster))
+    jpeg(paste0("plots/scatplot_km", num_clusts, "clusters.jpg"))
+    plot(data_pca3_bias, 
+         col = km_data_pca3_bias[[i]]$cluster, 
+         main = paste(num_clusts, "clusters:", deparse(substitute(km_data_pca3_bias))))
+    dev.off()
+}
 
 # Scree plot of within cluster sums of squares for each solution -------------------------------------
